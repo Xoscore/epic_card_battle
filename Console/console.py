90f44472751b6d9181a17821c7bf9bacbbebf419
Xoscore <gusarovon@gmail.com>
2020-04-21 13:07:38 +0700

    Add console

---
+++ Console/console.py
@@ -0,0 +1,19 @@
+# Make sure 'arial10x10.png' is in the same directory as this script.
+import tcod
+import tcod.event
+
+# Setup the font.
+tcod.console_set_custom_font(
+    "arial10x10.png",
+    tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,
+)
+# Initialize the root console in a context.
+with tcod.console_init_root(80, 60, order="F") as root_console:
+    root_console.print_(x=0, y=0, string='Hello World!')
+    while True:
+        tcod.console_flush()  # Show the console.
+        for event in tcod.event.wait():
+            if event.type == "QUIT":
+                raise SystemExit()
+    # The libtcod window will be closed at the end of this with-block.
+
